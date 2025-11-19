bbac72332fc499949f19d9180d5289cee9338ee2
mattis254 <matej.neoral@gmail.com>
2025-11-19 21:10:08 +0100

    Added timer and custom font

---
+++ AK9VH_Hra/Assets/Scripts/Stopky.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using TMPro;
+
+public class Stopky : MonoBehaviour
+{
+    public TextMeshProUGUI textCasovace; 
+    private float cas = 0f; 
+
+    void Update()
+    {
+        cas += Time.deltaTime;
+
+        float minuty = Mathf.FloorToInt(cas / 60); 
+        float vteriny = Mathf.FloorToInt(cas % 60);
+
+        textCasovace.text = string.Format("{0:00}:{1:00}", minuty, vteriny);
+    }
+}
